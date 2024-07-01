fadf8110973c046cdd4216a81576a68f1dd61fd1
Yathin Chandra <yathinchandra985@gmail.com>
2024-07-01 22:45:20 +0530

    userbackend is done

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const cors=require("cors");
 const app=express();
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(bodyParser.json())
+app.use(bodyparser.json())
 
 app.get("/",(req,res)=>{
   res.send("Home page");
