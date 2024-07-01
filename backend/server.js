b583aef93d3d1f6a07e443ac1c10974762f47230
Yathin Chandra <yathinchandra985@gmail.com>
2024-07-01 21:54:58 +0530

    starter-project

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const dotenv=require("dotenv").config();
+const express=require("express");
+const mongoose=require("mongoose");
+const bodyparser=require("body-parser");
+const cors=require("cors");
+
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(()=>{
+      app.listen(PORT,()=>{
+        console.log(`Server running on Port ${PORT}`)
+      })
+    })
+    .catch((err)=>console.log(err))
