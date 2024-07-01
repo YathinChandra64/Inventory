47d0268bc95a396a9bc00d15b630043d18a768c9
Yathin Chandra <yathinchandra985@gmail.com>
2024-07-01 22:31:21 +0530

    userModel created

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,14 @@ const bodyparser=require("body-parser");
 const cors=require("cors");
 
 const app=express();
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.get("/",(req,res)=>{
+  res.send("Home page");
+})
+
 const PORT=process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
